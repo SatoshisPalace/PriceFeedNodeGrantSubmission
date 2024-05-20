f36d75e9521e5c609461be0bd2137928e625a478
K <k@k.com>
2024-05-20 12:40:33 -0400

    logging and comment out betting

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import { logger } from 'satoshis-palace-reclaim-base'
 
 // Main function to initialize and continuously update contests and claims
 async function main(): Promise<void> {
+    logger.info('='.repeat(75));
+    logger.info(`RUNNING PRICE POSTING JOB: ${new Date().toISOString()}`);
+    logger.info('='.repeat(75));
 
     try {
         const timesToPost = await getTimesToPost();
@@ -21,8 +24,15 @@ async function main(): Promise<void> {
         logger.error('FATAL! Failed to post find prove and post prices', error)
     }
     // Schedule the next run
+    logger.info('='.repeat(75));
+    logger.info(`COMPLETED PRICE POSTING JOB: ${new Date().toISOString()}`);
+    logger.info('='.repeat(75));
     setTimeout(main, calculateNextRunDelay());
 }
 
 // Initial call to start the process
+logger.info('='.repeat(75))
+logger.info('STARTING CATCHUP')
+logger.info('='.repeat(75))
+
 main()
