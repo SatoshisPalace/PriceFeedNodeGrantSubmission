95239e926a4d4daeb930588229b4077109eb6ed4
K <k@k.com>
2024-06-24 22:42:27 -0400

    fix time

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ export const MAX_RETRIES = parseInt(process.env.MAX_RETRIES || '3'); // Default 
 export const COIN_ID = process.env.COIN_ID || 'BTC'; // Default to 'bitcoin' if not set
 export const DENOMINATED_COIN_ID = process.env.DENOMINATED_COIN_ID || 'USD';
 export const PRIVATE_KEY = process.env.PRIVATE_KEY
-export const ADDITIONAL_SECONDS = process.env.PRIVATE_KEY || 20; // 20 seconds past the interval
+export const ADDITIONAL_SECONDS = parseInt(process.env.PRIVATE_KEY || "20"); // 20 seconds past the interval
 
 export const INTERVAL_MINUTES = 5; // Interval of 5 minutes
 export const SECONDS_PER_MINUTE = 60; // Number of seconds in a minute
