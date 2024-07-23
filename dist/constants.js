815abfcdd2c2d63be9feb5d248c22648197c08d1
K <k@k.com>
2024-07-22 23:17:37 -0400

    build

--- dist/constants.js
+++ dist/constants.js
@@ -3,17 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SNIP20_CONTRACT_INFO = exports.PRICE_FEED_CONTRACT_INFO = exports.BULL_VS_BEAR_CONTRACT_INFO = exports.MILLISECONDS_PER_SECOND = exports.SECONDS_PER_MINUTE = exports.INTERVAL_MINUTES = exports.ADDITIONAL_SECONDS = exports.PRIVATE_KEY = exports.DENOMINATED_COIN_ID = exports.COIN_ID = exports.MAX_RETRIES = exports.REFRESH_INTERVAL = exports.TIME_OUT = void 0;
+exports.SNIP20_CONTRACT_INFO = exports.PRICE_FEED_CONTRACT_INFO = exports.BULL_VS_BEAR_CONTRACT_INFO = exports.MILLISECONDS_PER_SECOND = exports.SECONDS_PER_MINUTE = exports.INTERVAL_MINUTES = exports.ADDITIONAL_SECONDS = exports.DENOMINATED_COIN_ID = exports.COIN_ID = exports.MAX_RETRIES = exports.REFRESH_INTERVAL = exports.TIME_OUT = void 0;
 //src/constants.ts
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.TIME_OUT = 10000;
 exports.REFRESH_INTERVAL = parseInt(process.env.REFRESH_INTERVAL || '300000'); // Default to 5 minutes if not set
-exports.MAX_RETRIES = parseInt(process.env.MAX_RETRIES || '3'); // Default to 3 retries if not set
+exports.MAX_RETRIES = parseInt(process.env.MAX_RETRIES || '5'); // Default to 3 retries if not set
 exports.COIN_ID = process.env.COIN_ID || 1; // Default to 'bitcoin' if not set
 exports.DENOMINATED_COIN_ID = process.env.DENOMINATED_COIN_ID || 'USD';
-exports.PRIVATE_KEY = process.env.PRIVATE_KEY;
-exports.ADDITIONAL_SECONDS = parseInt(process.env.ADDITIONAL_SECONDS || "20"); // 20 seconds past the interval
+exports.ADDITIONAL_SECONDS = parseInt(process.env.ADDITIONAL_SECONDS || "40"); // 20 seconds past the interval
 exports.INTERVAL_MINUTES = 5; // Interval of 5 minutes
 exports.SECONDS_PER_MINUTE = 60; // Number of seconds in a minute
 exports.MILLISECONDS_PER_SECOND = 1000; // Number of milliseconds in a second
